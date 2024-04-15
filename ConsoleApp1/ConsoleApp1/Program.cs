79642ce6a72b212e46b6a6df6cd31b2189bf88e7
Nutelllaa <100772015+Nutelllaa@users.noreply.github.com>
2024-04-15 09:03:16 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Player
+{
+    public string Name { get; set; }
+    public List<int> Scores { get; set; }
+
+    public Player(string name, List<int> scores)
+    {
+        Name = name;
+        Scores = scores;
+    }
+
+    public int TotalScore()
+    {
+        return Scores.Sum();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Введення списку гравців та їх балів
+        List<Player> players = new List<Player>();
+        Console.WriteLine("Введіть інформацію про гравців:");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write($"Гравець {i + 1}: ");
+            string name = Console.ReadLine();
+            Console.Write("Бали за 3 останні гри (через пробіл): ");
+            List<int> scores = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            players.Add(new Player(name, scores));
+        }
+
+        // Відібрати топ 3 гравців за сумою балів
+        var topPlayers = players.OrderByDescending(player => player.TotalScore()).Take(3);
+
+        // Виведення результатів
+        Console.WriteLine("\nТоп 3 гравці за сумою балів:");
+        foreach (var player in topPlayers)
+        {
+            Console.WriteLine($"{player.Name} - {player.TotalScore()} балів");
+        }
+    }
+}
